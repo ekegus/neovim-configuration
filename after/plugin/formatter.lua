5b98f28779d484ee424f8a6532ce7f9b4f44d434
Ben Busch <ben.busch@breedr.co>
2023-10-30 21:30:14 +0000

    Add ormolu formatter

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,8 +1,17 @@
 -- https://github.com/mhartington/formatter.nvim
+
 local prettier = function()
 	return {
 		exe = "./node_modules/.bin/prettier",
 		args = { "--stdin-filepath", vim.fn.shellescape(vim.api.nvim_buf_get_name(0)) },
+		stdin = true,
+	}
+end
+
+local ormolu = function()
+	return {
+		exe = "ormolu",
+		args = { "", vim.fn.shellescape(vim.api.nvim_buf_get_name(0)) },
 		stdin = true,
 	}
 end
@@ -13,16 +22,17 @@ require("formatter").setup({
 		lua = {
 			require("formatter.filetypes.lua").stylua,
 		},
-		css = { prettier },
-		scss = { prettier },
+		-- css = { prettier },
+		-- scss = { prettier },
 		html = { prettier },
 		javascript = { prettier },
 		javascriptreact = { prettier },
 		typescript = { require("formatter.filetypes.typescript").prettierd },
 		typescriptreact = { require("formatter.filetypes.typescript").prettierd },
-		markdown = { prettier },
+		-- markdown = { prettier },
 		json = { prettier },
 		jsonc = { prettier },
+		haskell = { ormolu },
 		rust = { require("formatter.filetypes.rust").rustfmt() },
 
 		["*"] = {
@@ -32,6 +42,7 @@ require("formatter").setup({
 })
 
 vim.keymap.set("n", "<leader>F", ":Format<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fo", ":Format<CR>", { silent = true })
 
 -- Autocommand for formatting on save
 vim.api.nvim_exec(
