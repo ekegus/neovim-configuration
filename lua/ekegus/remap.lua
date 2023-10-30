ccbeee17859a1503824d68877f5efa9fd09a70f3
Ben Busch <ben.busch@breedr.co>
2023-10-30 21:48:57 +0000

    Update keymaps

--- lua/ekegus/remap.lua
+++ lua/ekegus/remap.lua
@@ -26,21 +26,27 @@ vim.keymap.set("n", "]b", ":bnext<CR>", { noremap = true })
 -- -------------------------Terminal-----------------------------
 vim.keymap.set("n", "<Leader>te", ":terminal<CR>", { noremap = true })
 
+-- Some Helix mappings I like
 vim.keymap.set("n", "mm", "<S-%>", { noremap = true })
 vim.keymap.set("n", "%", "<Nop>", { noremap = true })
-vim.keymap.set("n", "<S-%>", "<Nop>", { noremap = true })
-vim.keymap.set("i", "jk", "<ESC>", { noremap = true })
-vim.keymap.set("i", "<C-c>", "<Nop>", { noremap = true })
-vim.keymap.set("i", "<ESC>", "<Nop>", { noremap = true })
-vim.keymap.set("n", "0", "<Nop>", { noremap = true })
-
 vim.keymap.set("n", "gl", "<S-$>", { noremap = true })
 vim.keymap.set("n", "$", "<Nop>", { noremap = true })
 vim.keymap.set("n", "gh", "0", { noremap = true })
+vim.keymap.set("n", "0", "<Nop>", { noremap = true })
 vim.keymap.set("n", "gs", "<S-^>", { noremap = true })
 vim.keymap.set("n", "^", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "ge", "G", { noremap = true })
 vim.keymap.set("n", "G", "<Nop>", { noremap = true })
+vim.keymap.set("v", "mm", "<S-%>", { noremap = true })
+vim.keymap.set("v", "%", "<Nop>", { noremap = true })
+vim.keymap.set("v", "gl", "<S-$>", { noremap = true })
+vim.keymap.set("v", "$", "<Nop>", { noremap = true })
+vim.keymap.set("v", "gh", "0", { noremap = true })
+vim.keymap.set("v", "0", "<Nop>", { noremap = true })
+vim.keymap.set("v", "gs", "<S-^>", { noremap = true })
+vim.keymap.set("v", "^", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "ge", "G", { noremap = true })
+vim.keymap.set("v", "G", "<Nop>", { noremap = true })
 
 vim.keymap.set("n", "ga", "<C-6>", { noremap = true })
 vim.keymap.set("n", "<C-6>", "<Nop>", { noremap = true })
@@ -48,8 +54,15 @@ vim.keymap.set("n", "<C-6>", "<Nop>", { noremap = true })
 vim.keymap.set("t", "jk", "<C-\\><C-n>", { noremap = true })
 vim.keymap.set("t", "<C-\\><C-n>", "", { noremap = true })
 
--- Change next/prev: Doesn't seem to work properly
 -- https://learnvimscriptthehardway.stevelosh.com/chapters/15.html
+-- Use 'jk' instead of <ESC>
+vim.keymap.set("i", "jk", "<ESC>", { noremap = true })
+vim.keymap.set("i", "<C-c>", "<Nop>", { noremap = true })
+vim.keymap.set("i", "<ESC>", "<Nop>", { noremap = true })
+vim.keymap.set("v", "jk", "<ESC>", { noremap = true })
+vim.keymap.set("v", "<C-c>", "<Nop>", { noremap = true })
+vim.keymap.set("v", "<ESC>", "<Nop>", { noremap = true })
+-- Change inside p
 vim.keymap.set("o", "in(", ":<c-u>normal! f(vi(<cr>", { noremap = true })
 vim.keymap.set("o", "il(", ":<c-u>normal! F)vi(<cr>", { noremap = true })
 
@@ -61,3 +74,6 @@ vim.keymap.set("o", "il[", ":<c-u>normal! F]vi[<cr>", { noremap = true })
 
 vim.keymap.set("o", 'in"', ':<c-u>normal! f"vi"<cr>', { noremap = true })
 vim.keymap.set("o", 'il"', ':<c-u>normal! F"vi"<cr>', { noremap = true })
+
+-- Copy to system clipboard
+vim.keymap.set("v", "<Leader>y", '"*y', { noremap = true })
